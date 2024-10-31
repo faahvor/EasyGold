3ebd7c016f3baa74ffdd874c1335241d8afa83bf
faahvor <tinafavour2018@gmail.com>
2024-10-31 11:51:21 +0100

    me

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,14 +9,14 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full fixed bg-[#FDFBF4] ">
-      <div className="flex  max-w-[1280px]  justify-between items-center mx-auto ">
+    <div className="w-full fixed bg-[#FDFBF4]">
+      <div className="flex max-w-[1280px] justify-between items-center mx-auto">
         <img
           className="mt-2"
           src="https://res.cloudinary.com/dsjsvmug6/image/upload/v1723939712/logo_wyzjgn.png"
           alt=""
         />
-        <div className=" hidden font-acme lg:flex mt-3 *:font-semibold  gap-8 mr-[1rem]">
+        <div className="hidden font-acme lg:flex mt-3 *:font-semibold gap-8 mr-[1rem]">
           <a href="">About</a>
           <a href="">Programs</a>
           <a href="">Contacts</a>
@@ -24,7 +24,7 @@ const Navbar = () => {
 
         <div className="lg:hidden" onClick={navToggle}>
           <button
-            className={`z-40 mt-3 mr-6 block hamburger lg:hidden  focus:outline-none ${
+            className={`z-50 mt-3 mr-6 block hamburger lg:hidden focus:outline-none ${
               menuOpen ? "open" : ""
             }`}
           >
@@ -34,20 +34,34 @@ const Navbar = () => {
           </button>
         </div>
       </div>
+
+      {/* Sidebar */}
       <div
-        id="menu"
-        className={`bg-[#FDFBF4] fixed flex left-0 z-50 right-0 top-[3.5rem] font-acme *:font-semibold flex-col  rounded-xl px-6 pb-10 py-1 pt-10  space-y-3 text-lg text-black uppercase ${
-          menuOpen ? "flex" : "hidden"
-        }`}
+        className={`fixed top-0 right-0 bottom-0 z-40 bg-[#e6c4b0] text-[#0C394A] font-acme *:font-semibold flex flex-col px-8 pb-10 py-1 justify-center space-y-3 text-lg  uppercase transform ${
+          menuOpen ? "translate-x-0" : "translate-x-full"
+        } transition-transform duration-500 ease-in-out w-64`}
       >
-        <a className="flex " href="">
-          <span> Home</span>
-          <FaRegArrowAltCircleRight />
+        <img
+          className="mb-[2rem]"
+          src="https://res.cloudinary.com/dsjsvmug6/image/upload/v1723939712/logo_wyzjgn.png"
+          alt=""
+        />
+        <a className=" hover:text-[#E46924]" href="">
+        Home
         </a>
-        <a href="">About</a>
-        <a href="">Contacts</a>
+        <a className=" hover:text-[#E46924]"  href="">About</a>
+        <a className=" hover:text-[#E46924]"  href="">Contacts</a>
       </div>
+
+      {/* Overlay */}
+      {menuOpen && (
+        <div
+          className="fixed inset-0 bg-black opacity-50 z-30"
+          onClick={navToggle}
+        ></div>
+      )}
     </div>
   );
 };
+
 export default Navbar;
